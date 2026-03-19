e22249b8898d4d83d7196937ccd0e1597124d358
Caleb Herrera <46386938+griffygit@users.noreply.github.com>
2026-03-19 16:38:45 +0000

    Initialized Vite React and Created Initial Homepage

---
+++ my-portfolio/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1>Caleb Herrera</h1>
+      <p>Tech Ops + Emerging Web Developer</p>
+      <p>I build practical, clean, user-focused projects.</p>
+    </main>
+  );
+}
+
+export default App
